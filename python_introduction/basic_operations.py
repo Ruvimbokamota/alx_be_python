dd802db4464f467f12b61d0d6bdf9a2906f2a449
Petunia Ruvimbo Kamota <ruekamota@gmail.com>
2025-07-10 18:14:44 +0100

    created a basic_operations.py file with basic arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+#define variables
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+
+#difference of numbers
+difference = number1 - number2
+
+#product
+product = number1 * number2
+
+print("Addition of 10 and 5 is", number1 + number2)
+print("Subtraction of 10 and 5 is", number1 - number2)
+print("Multiplication of 10 and 5 is", number1*number2)
+
